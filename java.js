85df2690da7430763f4679e279d4a4ed5a556981
shemalucien <shemalucien5@gmail.com>
2021-12-16 14:13:02 +0200

    Uploading Project

---
+++ java.js
@@ -0,0 +1,15 @@
+function myfocus(x)
+{
+	x.style.background=yellow;
+}
+function myblur()
+{
+	var x=document.getElementById("email");
+	x.value=x.value.toLowerCase();
+}
+function message()
+{
+	var choice=prompt("What is your name?");
+	alert("you are welcome");
+}
+	
